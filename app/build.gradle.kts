2881143369fa37a0235fb7d79452982f8aaf20b4
imdtouch <imdtouch@users.noreply.github.com>
2026-01-07 20:04:48 -0500

    v1.4 - Test update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.jci_andriod12"
         minSdk = 31
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
